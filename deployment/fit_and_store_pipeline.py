8a32fb30850d39a9eca084eaaf2d8d209acdf2e8
Tomas Singliar <singliar@hotmail.com>
2018-06-08 15:59:39 -0700

    local structuring of the pipeline

--- deployment/fit_and_store_pipeline.py
+++ deployment/fit_and_store_pipeline.py
@@ -48,42 +48,60 @@ class AvgWordVectorFeaturizer(object):
 
 ###
 
-training_set_file = "training_set_01.csv"
-text_data_file = "attack_data.csv"
-w2v_file = 'glove_6B_50d_w2v.txt' # convert glove file to w2v format using gensim.scripts.glove2word2vec
-# glove_file = 'glove.6B.50d.txt'
-# from gensim.scripts.glove2word2vec import glove2word2vec
-# glove2word2vec(glove_file, w2v_file)
+def create_small_w2v_file():
+    glove_file = 'E:/Projects/MLADS18S/glove.6B.50d.txt'
+    # convert glove file to w2v format using gensim.scripts.glove2word2vec
+    w2v_file = './glove_6B_50d_w2v.txt' 
+    from gensim.scripts.glove2word2vec import glove2word2vec
+    glove2word2vec(glove_file, w2v_file)
 
-word_vectors = KeyedVectors.load_word2vec_format(w2v_file, binary=False)
+def create_train_test_split():
+    
+    training_set_file = "training_set_01.csv"
+    text_data_file = "attack_data.csv"
 
-text_data = pd.read_csv(text_data_file, encoding='windows-1252')
-text_data = text_data.set_index("rev_id")
+    word_vectors = KeyedVectors.load_word2vec_format(w2v_file, binary=False)
 
-training_set_rev_ids = pd.read_csv(training_set_file).rev_id
-test_candidate_rev_ids = set.difference(set(text_data.index.values), set(training_set_rev_ids))
-test_set_rev_ids = random.sample(test_candidate_rev_ids, 1000)
+    text_data = pd.read_csv(text_data_file, encoding='windows-1252')
+    text_data = text_data.set_index("rev_id")
 
-training_data = text_data.loc[training_set_rev_ids]
-test_data = text_data.loc[test_set_rev_ids]
+    training_set_rev_ids = pd.read_csv(training_set_file).rev_id
+    test_candidate_rev_ids = set.difference(set(text_data.index.values), set(training_set_rev_ids))
+    
+    # take a random sample of 1000 ids for test set
+    test_set_rev_ids = random.sample(test_candidate_rev_ids, 1000)
+    
+    training_data = text_data.loc[training_set_rev_ids]
+    test_data = text_data.loc[test_set_rev_ids]
+    return training_data, test_data
+
+def train():
+    """
+    Creates a pickled trained model.
+    """
+
+    model_pipeline = Pipeline([
+        ('preprocessor', GensimPreprocessor()),
+        ('vectorizer', AvgWordVectorFeaturizer(embedding=word_vectors)),
+        ('classifier', RandomForestClassifier(n_estimators=100, oob_score=True, random_state=1)),
+    ])
+
+    fitted_model = model_pipeline.fit(training_data.comment, [int(x) for x in training_data.is_attack])
+    return fitted_model
+
+    pred = fitted_model.predict(test_data.comment)
+    confusion_matrix([int(x) for x in test_data.is_attack], pred)
+    
+    joblib.dump(fitted_model, 'rf_attack_classifier_pipeline.pkl') 
 
 
-model_pipeline = Pipeline([
-    ('preprocessor', GensimPreprocessor()),
-    ('vectorizer', AvgWordVectorFeaturizer(embedding=word_vectors)),
-    ('classifier', RandomForestClassifier(n_estimators=100, oob_score=True, random_state=1)),
-])
+def init():
+    """
+    Init function of the scoring script
+    """"
 
-fitted_model = model_pipeline.fit(training_data.comment, [int(x) for x in training_data.is_attack])
-
-pred = fitted_model.predict(test_data.comment)
-
-confusion_matrix([int(x) for x in test_data.is_attack], pred)
-
-joblib.dump(fitted_model, 'rf_attack_classifier_pipeline.pkl') 
+    reloaded_model = joblib.load('rf_attack_classifier_pipeline.pkl')
 
 
-###
-
-reloaded_model = joblib.load('rf_attack_classifier_pipeline.pkl')
-reloaded_model.predict(['You are scum.', 'I like your shoes.', 'You are pxzx.'])
+def run():
+    reloaded_model.predict(['You are scum.', 'I like your shoes.', 'You are pxzx.'])
