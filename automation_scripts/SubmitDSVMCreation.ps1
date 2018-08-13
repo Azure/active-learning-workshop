bab71412560582f03eb84b3c4ca51cf5b6992908
Mario Inchiosa <marinch@microsoft.com>
2018-08-13 15:23:16 -0700

    change obsolete Select-AzureRmProfile

--- automation_scripts/SubmitDSVMCreation.ps1
+++ automation_scripts/SubmitDSVMCreation.ps1
@@ -3,7 +3,7 @@ param([string]$clustername, [string]$resourcegroup, [string]$profilepath, [strin
 function SubmitDSVMCreation {
     param([string]$clustername, [string]$resourcegroup, [string]$clusterpasswd, [string]$sshusername)
 	$templatePath = "$currentPath\template.json";
-    Select-AzureRmProfile -Path $profilepath;
+    Select-AzureRmContext -Path $profilepath;
 	$dsvmparams = @{rg=$resourcegroup;storageAccountType="Premium_LRS";location="westus";virtualMachineSize="Standard_DS12_v2";virtualMachineName=$clustername;adminUsername=$sshusername;virtualNetworkName=$clustername+"-vnet";networkInterfaceName=$clustername+"359";networkSecurityGroupName=$clustername+"-ng";adminPassword=$clusterpasswd;storageAccountName=$clustername+"disk";publicIpAddressName=$clustername+"-ip"};
 	New-AzureRmResourceGroupDeployment -Name $clustername -ResourceGroupName $resourcegroup -TemplateFile $templatePath -TemplateParameterObject $dsvmparams;
 }
